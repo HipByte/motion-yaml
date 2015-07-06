34d3c46d45308a3781c5b6b8f810a8aadcd02718
Watson <watson1978@gmail.com>
2015-07-07 06:15:48 +0900

    add more specs for type converting

--- spec/type_spec.rb
+++ spec/type_spec.rb
@@ -1,35 +1,48 @@
-describe "Decimal" do
+describe "Type Converting" do
 
-  it "should be converted to Fixnum object" do
-    obj = YAML.load(<<END)
-decimal1:  123
-float1:    0.5
-END
-    obj.should == {"decimal1"=>123, "float1"=>0.5}
+  describe "Decimal" do
+    it "should be converted to Fixnum object" do
+      obj = YAML.load("decimal: 123")
+      obj.should == {"decimal"=>123}
+    end
   end
 
-end
+  describe "Float" do
+    it "should be converted to Float object" do
+      obj = YAML.load("float: 123.5")
+      obj.should == {"float"=>123.5}
 
-describe "Octal" do
+      obj = YAML.load("float: .123")
+      obj.should == {"float"=>0.123}
 
-  it "should be converted to Fixnum object" do
-    obj = YAML.load("octal: 0644")
-    obj.should == {"octal"=>420}
+      obj = YAML.load("float: 123.5.6")
+      obj.should == {"float"=>"123.5.6"}
+    end
   end
 
-end
-
-describe "Hex" do
-
-  it "should be converted to Fixnum object" do
-    obj = YAML.load("hex: 0xff")
-    obj.should == {"hex"=>255}
-
-    obj = YAML.load("hex: 0xBEEF")
-    obj.should == {"hex"=>48879}
-
-    obj = YAML.load("hex: xBEEF")
-    obj.should == {"hex"=>"xBEEF"}
+  describe "Octal" do
+    it "should be converted to Fixnum object" do
+      obj = YAML.load("octal: 0644")
+      obj.should == {"octal"=>420}
+    end
   end
 
+  describe "Hex" do
+    it "should be converted to Fixnum object" do
+      obj = YAML.load("hex: 0xff")
+      obj.should == {"hex"=>255}
+
+      obj = YAML.load("hex: 0x9Ac")
+      obj.should == {"hex"=>2476}
+
+      obj = YAML.load("hex: 0xBEEF")
+      obj.should == {"hex"=>48879}
+
+      obj = YAML.load("hex: 0xxBEEF")
+      obj.should == {"hex"=>"0xxBEEF"}
+
+      obj = YAML.load("hex: xBEEF")
+      obj.should == {"hex"=>"xBEEF"}
+    end
+  end
 end
