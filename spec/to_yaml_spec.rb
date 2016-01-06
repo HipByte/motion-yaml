b757731f9b047bc895f5bffbab15dcc28cbbf513
Watson <watson1978@gmail.com>
2016-01-06 11:30:52 +0900

    [fix #14] dump quoted string when it will be converted with #load method

--- spec/to_yaml_spec.rb
+++ spec/to_yaml_spec.rb
@@ -1,12 +1,16 @@
 describe "#to_yaml" do
 
   before do
-    @list = [{ 'a' => 'b' }, 'foo']
+    @list = [{ 'a' => 'b' }, 'foo', 'No', 'yeS']
   end
 
   it "should work" do
     @list.to_yaml.should == YAML.dump(@list)
     YAML.load(@list.to_yaml).should == @list
+  end
+
+  it "should dump quoted string if string will be converted with #load method" do
+    @list.to_yaml.should == "- a: b\n- foo\n- 'No'\n- 'yeS'\n"
   end
 
 end
